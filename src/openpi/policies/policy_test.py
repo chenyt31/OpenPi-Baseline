ab66d3d37ee1daebd2352d0f0cce675b1e44f3fa
Ury Zhilinsky <ury@physicalintelligence.company>
2024-11-15 11:09:24 -0800

    Move aloha code to a shared module

--- src/openpi/policies/policy_test.py
+++ src/openpi/policies/policy_test.py
@@ -1,185 +1,24 @@
-import pathlib
-
-import numpy as np
-
-from openpi import transforms
-from openpi.models import model as _model
-from openpi.models import pi0
-from openpi.models import tokenizer
+from openpi.policies import aloha_policy
 from openpi.policies import policy as _policy
 
 
-def load_pi0_model() -> _model.Model:
-    model = _model.Model(pi0.Module())
-    return _model.restore_params(model, pathlib.Path("checkpoints/pi0_base/model").absolute())
-
-
-def create_aloha_policy(model: _model.Model) -> _policy.Policy:
-    norm_stats = make_aloha_norm_stats()
-    return _policy.Policy(
-        model,
-        transforms=[
-            transforms.AlohaInputs(action_dim=model.action_dim),
-            transforms.TokenizePrompt(tokenizer.PaligemmaTokenizer()),
-            transforms.Normalize(norm_stats),
-        ],
-        output_transforms=[
-            transforms.Unnormalize(norm_stats),
-            transforms.AlohaOutputs(),
-        ],
-    )
-
-
-def make_aloha_example() -> dict:
-    image = np.random.randint(256, size=(480, 640, 3), dtype=np.uint8)
-
-    return {
-        "observation/image/cam_left_wrist": image,
-        "observation/image/cam_right_wrist": image,
-        "observation/image/cam_high": image,
-        "observation/image/cam_low": image,
-        "observation/qpos": np.ones((14,)),
-    }
-
-
-def make_aloha_norm_stats():
-    return {
-        "actions": transforms.NormStats(
-            mean=np.array(
-                [
-                    -0.0,
-                    0.01,
-                    0.02,
-                    0.0,
-                    -0.01,
-                    0.0,
-                    0.44,
-                    -0.0,
-                    0.02,
-                    0.02,
-                    0.0,
-                    -0.01,
-                    0.0,
-                    0.43,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-            std=np.array(
-                [
-                    0.11,
-                    0.17,
-                    0.16,
-                    0.11,
-                    0.17,
-                    0.15,
-                    0.41,
-                    0.12,
-                    0.19,
-                    0.18,
-                    0.14,
-                    0.19,
-                    0.19,
-                    0.39,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-        ),
-        "state": transforms.NormStats(
-            mean=np.array(
-                [
-                    0.11,
-                    0.45,
-                    -0.6,
-                    0.13,
-                    0.5,
-                    -0.2,
-                    0.45,
-                    -0.07,
-                    0.31,
-                    -0.48,
-                    -0.13,
-                    0.55,
-                    0.21,
-                    0.44,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-            std=np.array(
-                [
-                    0.3,
-                    0.54,
-                    0.55,
-                    0.28,
-                    0.47,
-                    0.44,
-                    0.38,
-                    0.32,
-                    0.57,
-                    0.56,
-                    0.3,
-                    0.49,
-                    0.54,
-                    0.36,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-        ),
-    }
-
-
 def test_infer():
-    model = load_pi0_model()
-    policy = create_aloha_policy(model)
+    model = aloha_policy.load_pi0_model()
+    policy = aloha_policy.create_aloha_policy(model)
 
-    outputs = policy.infer(make_aloha_example())
+    outputs = policy.infer(aloha_policy.make_aloha_example())
     assert outputs["action/qpos"].shape == (model.action_horizon, 14)
 
 
 def test_broker():
-    model = load_pi0_model()
+    model = aloha_policy.load_pi0_model()
     policy = _policy.ActionChunkBroker(
-        create_aloha_policy(model),
+        aloha_policy.create_aloha_policy(model),
         # Only execute the first half of the chunk.
         action_horizon=model.action_horizon // 2,
     )
 
-    example = make_aloha_example()
+    example = aloha_policy.make_aloha_example()
     for _ in range(model.action_horizon):
         outputs = policy.infer(example)
         assert outputs["action/qpos"].shape == (14,)
--- src/openpi/serving/http_policy_server_pi0.py
+++ src/openpi/serving/http_policy_server_pi0.py
@@ -1,165 +1,21 @@
 import logging
-import pathlib
 
-import numpy as np
 import tyro
 
-from openpi import transforms
-from openpi.models import model as _model
-from openpi.models import pi0
-from openpi.models import tokenizer
+from openpi.policies import aloha_policy
 from openpi.policies import policy as _policy
 from openpi.serving import http_policy_server
-
-
-def load_pi0_model() -> _model.Model:
-    model = _model.Model(pi0.Module(), tokenizer=tokenizer.PaligemmaTokenizer())
-    return _model.restore_params(model, pathlib.Path("checkpoints/pi0_base/model").absolute())
-
-
-def make_aloha_norm_stats():
-    """Define the normalization stats for ALOHA."""
-    return {
-        "actions": transforms.NormStats(
-            mean=np.array(
-                [
-                    -0.0,
-                    0.01,
-                    0.02,
-                    0.0,
-                    -0.01,
-                    0.0,
-                    0.44,
-                    -0.0,
-                    0.02,
-                    0.02,
-                    0.0,
-                    -0.01,
-                    0.0,
-                    0.43,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-            std=np.array(
-                [
-                    0.11,
-                    0.17,
-                    0.16,
-                    0.11,
-                    0.17,
-                    0.15,
-                    0.41,
-                    0.12,
-                    0.19,
-                    0.18,
-                    0.14,
-                    0.19,
-                    0.19,
-                    0.39,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-        ),
-        "state": transforms.NormStats(
-            mean=np.array(
-                [
-                    0.11,
-                    0.45,
-                    -0.6,
-                    0.13,
-                    0.5,
-                    -0.2,
-                    0.45,
-                    -0.07,
-                    0.31,
-                    -0.48,
-                    -0.13,
-                    0.55,
-                    0.21,
-                    0.44,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-            std=np.array(
-                [
-                    0.3,
-                    0.54,
-                    0.55,
-                    0.28,
-                    0.47,
-                    0.44,
-                    0.38,
-                    0.32,
-                    0.57,
-                    0.56,
-                    0.3,
-                    0.49,
-                    0.54,
-                    0.36,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                ]
-            ),
-        ),
-    }
 
 
 def main(
     port: int = 8000,
 ) -> None:
     logging.info("Loading model...")
-    model = load_pi0_model()
-
-    norm_stats = make_aloha_norm_stats()
+    model = aloha_policy.load_pi0_model()
 
     logging.info("Creating policy...")
     policy = _policy.ActionChunkBroker(
-        _policy.Policy(
-            model,
-            transforms=[
-                transforms.AlohaInputs(action_dim=model.action_dim),
-                transforms.Normalize(norm_stats),
-            ],
-            output_transforms=[
-                transforms.Unnormalize(norm_stats),
-                transforms.AlohaOutputs(),
-            ],
-        ),
+        aloha_policy.create_aloha_policy(model),
         # Only execute the first half of the chunk.
         action_horizon=model.action_horizon // 2,
     )

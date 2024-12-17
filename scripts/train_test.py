074361e30115aa09006b703050a4b87ffc7cc132
Kevin Black <kevin3.black@gmail.com>
2024-12-17 14:32:43 -0800

    Add pizero_small to test

--- scripts/train_test.py
+++ scripts/train_test.py
@@ -1,14 +1,18 @@
 import dataclasses
 import pathlib
 
+import pytest
+
 from openpi.training import config as _config
 
 from . import train
 
 
-def test_train(tmp_path: pathlib.Path):
+@pytest.mark.parametrize("config_name", ["debug", "pi0_small"])
+def test_train(tmp_path: pathlib.Path, config_name: str):
     config = dataclasses.replace(
-        _config.CONFIGS["debug"],
+        _config.CONFIGS[config_name],
+        batch_size=2,
         checkpoint_dir=tmp_path / "checkpoint",
         overwrite=False,
         resume=False,

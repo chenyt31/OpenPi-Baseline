4bd9fd1df1e57847ee10754ad66734df0b05599f
Ury Zhilinsky <ury@physicalintelligence.company>
2024-12-22 18:46:14 -0800

    Fix check

--- packages/openpi-client/src/openpi_client/msgpack_numpy.py
+++ packages/openpi-client/src/openpi_client/msgpack_numpy.py
@@ -19,7 +19,7 @@ import numpy as np
 
 
 def pack_array(obj):
-    if (isinstance(obj, np.ndarray) or isinstance(np.generic)) and obj.dtype.kind in ("V", "O", "c"):
+    if (isinstance(obj, (np.ndarray, np.generic))) and obj.dtype.kind in ("V", "O", "c"):
         raise ValueError(f"Unsupported dtype: {obj.dtype}")
 
     if isinstance(obj, np.ndarray):

ac44a004f756def6660d43eaf49cc30ab2d4476e
Ury Zhilinsky <ury@physicalintelligence.company>
2025-01-01 19:14:12 +0000

    Utilities to convert from internal checkpoints

--- src/openpi/transforms_test.py
+++ src/openpi/transforms_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import openpi.models.tokenizer as _tokenizer
 import openpi.transforms as _transforms
@@ -86,3 +87,29 @@ def test_tokenize_prompt_default():
     tok_prompt, tok_mask = tokenizer.tokenize("This is a default prompt")
     assert np.allclose(tok_prompt, data["tokenized_prompt"])
     assert np.allclose(tok_mask, data["tokenized_prompt_mask"])
+
+
+def test_transform_dict():
+    # Rename and remove keys.
+    input = {"a": {"b": 1, "c": 2}}
+    output = _transforms.transform_dict({"a/b": "a/c", "a/c": None}, input)
+    assert output == {"a": {"c": 1}}
+
+    # Raises and error since the renamed key conflicts with an existing key.
+    with pytest.raises(ValueError, match="Key 'a/c' already exists in output"):
+        _transforms.transform_dict({"a/b": "a/c"}, input)
+
+    # Full match is required and so nothing will be removed.
+    input = {"a": {"b": 1, "c": 2}}
+    output = _transforms.transform_dict({"a": None}, input)
+    assert output == input
+
+    # The regex matches the entire key and so the entire input will be removed.
+    input = {"a": {"b": 1, "c": 2}}
+    output = _transforms.transform_dict({"a.+": None}, input)
+    assert output == {}
+
+    # Replace keys using backreferences. All leaves named 'c' are replaced with 'd'.
+    input = {"a": {"b": 1, "c": 1}, "b": {"c": 2}}
+    output = _transforms.transform_dict({"(.+)/c": r"\1/d"}, input)
+    assert output == {"a": {"b": 1, "d": 1}, "b": {"d": 2}}

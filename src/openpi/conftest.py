ce9840c13335308fb7ca26bc24393061e1245173
Ury Zhilinsky <ury@physicalintelligence.company>
2025-01-08 19:00:12 -0800

    Force JAX to use CPU mode only if a GPU is not detected

--- src/openpi/conftest.py
+++ src/openpi/conftest.py
@@ -1,7 +1,17 @@
 import os
 
+import pynvml
 import pytest
 
 
+def set_jax_cpu_backend_if_no_gpu() -> None:
+    try:
+        pynvml.nvmlInit()
+        pynvml.nvmlShutdown()
+    except pynvml.NVMLError:
+        # No GPU found.
+        os.environ["JAX_PLATFORMS"] = "cpu"
+
+
 def pytest_configure(config: pytest.Config) -> None:
-    os.environ["JAX_PLATFORMS"] = "cpu"
+    set_jax_cpu_backend_if_no_gpu()

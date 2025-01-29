3063c1eddaaa7a7a474de1e91fef8b9e15ecc0a6
Ury Zhilinsky <ury@physicalintelligence.company>
2025-01-29 12:32:26 -0800

    Switch to new LoRA implementation

--- src/openpi/models/gemma.py
+++ src/openpi/models/gemma.py
@@ -25,18 +25,17 @@ We follow this einsum axis naming convention:
   D: d_model ("features")
 """
 
-from collections.abc import Callable, Sequence
+from collections.abc import Sequence
 import dataclasses
 import logging
-import math
 from typing import Literal, TypeAlias
 
 import einops
 import flax.linen as nn
-import flax.traverse_util as traverse_util
 import jax
 import jax.numpy as jnp
 
+import openpi.models.lora as lora
 import openpi.shared.array_typing as at
 import openpi.training.sharding as sharding
 
@@ -67,9 +66,7 @@ class Config:
     num_heads: int
     num_kv_heads: int
     head_dim: int
-    projection_lora: LoRAConfig | None = None
-    projection_kv_lora: LoRAConfig | None = None
-    output_lora: LoRAConfig | None = None
+    lora_configs: dict[str, lora.LoRAConfig] = dataclasses.field(default_factory=dict)
 
 
 Variant = Literal["dummy", "gemma_300m", "gemma_2b", "gemma_2b_lora"]
@@ -113,23 +110,9 @@ def get_config(variant: Variant) -> Config:
             num_heads=8,
             num_kv_heads=1,
             head_dim=256,
-            projection_lora=LoRAConfig(rank=64, alpha=64.0),
-            projection_kv_lora=LoRAConfig(rank=64, alpha=64.0),
-            output_lora=LoRAConfig(rank=64, alpha=64.0),
+            lora_configs={"default": lora.LoRAConfig(rank=64, alpha=64.0)},
         )
     raise ValueError(f"Unknown variant: {variant}")
-
-
-@at.typecheck
-class Einsum(nn.Module):
-    shape: tuple[int, ...]
-    init_fn: nn.initializers.Initializer
-
-    @nn.compact
-    def __call__(self, eqn, x):
-        dtype = x.dtype  # original dtype, could be half-precision
-        w = self.param("w", self.init_fn, self.shape).astype(dtype)
-        return jnp.einsum(eqn, x, w)
 
 
 @at.typecheck
@@ -189,48 +172,27 @@ class Attention(nn.Module):
             if x is None:
                 continue
             if config.num_kv_heads == config.num_heads:
-                qkv_einsum = Einsum(
+                qkv_einsum = lora.Einsum(
                     shape=(3, config.num_heads, config.width, config.head_dim),
                     name=_name("qkv_einsum", i),
                     init_fn=nn.initializers.lecun_normal(in_axis=-2, out_axis=-1, batch_axis=(0, 1)),
+                    lora_config=config.lora_configs.get("default"),
                 )
-                if config.projection_lora is not None:
-                    qkv_einsum = LoRAEinsum(
-                        qkv_einsum,
-                        config.projection_lora,
-                        merge_eqn="3KDL,3KLKH->3KDH",
-                        lora_a_init_fn=nn.initializers.lecun_normal(in_axis=-3, out_axis=-1, batch_axis=(0, 1, 3)),
-                        lora_b_init_fn=nn.initializers.lecun_normal(in_axis=-3, out_axis=-1, batch_axis=(0, 1, 3)),
-                    )
                 qkvs.append(qkv_einsum("BSD,3KDH->3BSKH", x))
             else:
-                q_einsum = Einsum(
+                q_einsum = lora.Einsum(
                     shape=(config.num_heads, config.width, config.head_dim),
                     name=_name("q_einsum", i),
                     init_fn=nn.initializers.lecun_normal(in_axis=-2, out_axis=-1, batch_axis=(0,)),
+                    lora_config=config.lora_configs.get("default"),
                 )
-                if config.projection_lora is not None:
-                    q_einsum = LoRAEinsum(
-                        q_einsum,
-                        config.projection_lora,
-                        merge_eqn="NDL,NLNH->NDH",
-                        lora_a_init_fn=nn.initializers.lecun_normal(in_axis=-2, out_axis=-1, batch_axis=(0,)),
-                        lora_b_init_fn=nn.initializers.lecun_normal(in_axis=-3, out_axis=-1, batch_axis=(0, 2)),
-                    )
                 q = q_einsum("BTD,NDH->BTNH", x)
-                kv_einsum = Einsum(
+                kv_einsum = lora.Einsum(
                     shape=(2, config.num_kv_heads, config.width, config.head_dim),
                     name=_name("kv_einsum", i),
                     init_fn=nn.initializers.lecun_normal(in_axis=-2, out_axis=-1, batch_axis=(0, 1)),
+                    lora_config=config.lora_configs.get("default"),
                 )
-                if config.projection_kv_lora is not None:
-                    kv_einsum = LoRAEinsum(
-                        kv_einsum,
-                        config.projection_kv_lora,
-                        merge_eqn="2KDL,2KLKH->2KDH",
-                        lora_a_init_fn=nn.initializers.lecun_normal(in_axis=-2, out_axis=-1, batch_axis=(0, 1)),
-                        lora_b_init_fn=nn.initializers.lecun_normal(in_axis=-3, out_axis=-1, batch_axis=(0, 1, 3)),
-                    )
                 k, v = kv_einsum("BSD,2KDH->2BSKH", x)
                 qkvs.append((q, k, v))
 
@@ -271,19 +233,12 @@ class Attention(nn.Module):
         for i, (x, config) in enumerate(zip(xs, self.configs, strict=True)):
             if x is not None:
                 end = start + x.shape[1]
-                out_einsum = Einsum(
+                out_einsum = lora.Einsum(
                     shape=(config.num_heads, config.head_dim, config.width),
                     name=_name("attn_vec_einsum", i),
                     init_fn=nn.initializers.lecun_normal(in_axis=(-3, -2), out_axis=-1),
+                    lora_config=config.lora_configs.get("default"),
                 )
-                if config.projection_lora is not None:
-                    out_einsum = LoRAEinsum(
-                        out_einsum,
-                        config.projection_lora,
-                        merge_eqn="NHNL,NLD->NHD",
-                        lora_a_init_fn=nn.initializers.lecun_normal(in_axis=(-4, -3), out_axis=(-2, -1)),
-                        lora_b_init_fn=nn.initializers.lecun_normal(in_axis=(-3, -2), out_axis=-1),
-                    )
                 out.append(out_einsum("BTNH,NHD->BTD", encoded[:, start:end]))
                 start = end
             else:
@@ -474,131 +429,3 @@ def _name(name, i):
     if i == 0:
         return name
     return f"{name}_{i}"
-
-
-_LORA_A_KEY = "lora_a"
-_LORA_B_KEY = "lora_b"
-
-
-@at.typecheck
-class LoRAEinsum(nn.Module):
-    base: Einsum
-    lora_config: LoRAConfig
-    merge_eqn: str
-    lora_a_init_fn: nn.initializers.Initializer
-    lora_b_init_fn: nn.initializers.Initializer
-
-    def setup(self):
-        nn.share_scope(self, self.base)
-
-    @nn.compact
-    def __call__(self, eqn, x, *, deterministic=True):
-        orig_x = x
-        eqn_lora_a, eqn_lora_b = self._get_lora_eqn(eqn, self.merge_eqn)
-        if self.lora_config.dropout > 0.0:
-            x = nn.Dropout(rate=self.lora_config.dropout, deterministic=deterministic)(x)
-        lora_a_shape, lora_b_shape = self._parse_shape(self.merge_eqn)
-        lora_a = self.param(_LORA_A_KEY, self.lora_a_init_fn, lora_a_shape).astype(x.dtype)
-        lora_b = self.param(_LORA_B_KEY, self.lora_b_init_fn, lora_b_shape).astype(x.dtype)
-        lora_a = jnp.einsum(eqn_lora_a, x, lora_a)
-        lora_b = jnp.einsum(eqn_lora_b, lora_a, lora_b)
-
-        # TODO: scaling_value should ideally be a self.variable however currently base model doesn't allow any
-        # auxilary variables.
-        scaling_value = (
-            self.lora_config.alpha / self.lora_config.rank
-            if not self.lora_config.rslora
-            else self.lora_config.alpha / math.sqrt(self.lora_config.rank)
-        )
-
-        return self.base(eqn, orig_x) + lora_b * scaling_value
-
-    def _get_lora_eqn(self, eqn: str, lora_merge_eqn: str) -> tuple[str, str]:
-        """Figure out lora_a and lora_b eqn from eqn and lora_merge_eqn.
-        input:
-        eqn: x,w->y
-        lora_merge_eqn: lora_a,lora_b->w
-
-        output:
-        lora_a_eqn: x,lora_a->?
-        lora_b_eqn: ?,lora_b->y
-        """
-        (x_repr, w_repr), y_repr = _parse_einops_eqn(eqn)
-        (lora_a_repr, lora_b_repr), w_repr_p = _parse_einops_eqn(lora_merge_eqn)
-        assert len(w_repr) == len(self.base.shape), f"w_repr={w_repr}, shape={self.base.shape}"
-        assert w_repr == w_repr_p, f"w_repr={w_repr}, w_repr_p={w_repr_p} should be the same."
-
-        # figure out x,lora_a's output annotation by using y and lora_b
-        # the way to do this is to:
-        # 1. remove the common prefix and suffix from lora_b and y
-        # 2. then the ? will be (common prefix) (stripped y) (stripped lora_b)
-        # the equation will look like:
-        # [(prefix) (stripped y) (lora b)], [(prefix) (lora b) (suffix)] -> [(prefix) (y) (suffix)]
-        prefix, _, y_repr_stripped, lora_b_repr_stripped = self._remove_common_prefix_suffix(y_repr, lora_b_repr)
-        lora_intermediate_repr = prefix + y_repr_stripped + lora_b_repr_stripped
-
-        eqn_lora_a_lhs = ", ".join([x_repr, lora_a_repr])
-        eqn_lora_b_lhs = ", ".join([lora_intermediate_repr, lora_b_repr])
-        return eqn_lora_a_lhs + " -> " + lora_intermediate_repr, eqn_lora_b_lhs + " -> " + y_repr
-
-    def _remove_common_prefix_suffix(self, str1, str2):
-        # Get the common prefix
-        prefix = ""
-        for i in range(min(len(str1), len(str2))):
-            if str1[i] == str2[i]:
-                prefix += str1[i]
-            else:
-                break
-
-        # Get the common suffix
-        suffix = ""
-        for i in range(1, min(len(str1), len(str2)) + 1):
-            if str1[-i] == str2[-i]:
-                suffix = str1[-i] + suffix
-            else:
-                break
-
-        return prefix, suffix, str1[len(prefix) : -len(suffix)], str2[len(prefix) : -len(suffix)]
-
-    def _parse_shape(self, lora_merge_eqn: str) -> tuple[tuple[int, ...], tuple[int, ...]]:
-        (lora_lhs_part_0, lora_lhs_part_1), lora_rhs = _parse_einops_eqn(lora_merge_eqn)
-        ann_to_dim = dict(zip(lora_rhs, self.base.shape, strict=True))
-        ann_to_dim[self.lora_config.rank_annotation] = self.lora_config.rank
-        return tuple(ann_to_dim[ann] for ann in lora_lhs_part_0), tuple(ann_to_dim[ann] for ann in lora_lhs_part_1)
-
-
-def merge_lora_params(lora_params: at.PyTree, get_lora_transform_eqn: Callable[[str], str]) -> at.PyTree:
-    params = lora_params["params"]
-    flattened_params = traverse_util.flatten_dict(params, sep="/")
-    merged_params = {}
-    for k in flattened_params:
-        if _LORA_A_KEY not in k:
-            continue
-        lora_b_key = k.replace(_LORA_A_KEY, _LORA_B_KEY)
-        orig_w_key = k.replace(_LORA_A_KEY, "w")
-        assert lora_b_key in flattened_params
-        assert orig_w_key in flattened_params
-        lora_merge = jnp.einsum(get_lora_transform_eqn(k), flattened_params[k], flattened_params[lora_b_key])
-        # TODO: Currently we don't handling lora scaling value here due to the base model doesn't support auxilary
-        # variables.
-        merged_params[orig_w_key] = flattened_params[orig_w_key] + lora_merge
-    for k in flattened_params:
-        if _LORA_A_KEY in k or _LORA_B_KEY in k:
-            continue
-        if k not in merged_params:
-            merged_params[k] = flattened_params[k]
-    return {"params": traverse_util.unflatten_dict(merged_params, sep="/")}
-
-
-def _parse_einops_eqn(eqn: str) -> tuple[tuple[str, str], str]:
-    lhs, rhs = eqn.split("->")
-    lhs_parts = lhs.split(",")
-    assert len(lhs_parts) == 2
-
-    def strip_space(s):
-        return s.replace(" ", "")
-
-    lhs_parts[0] = strip_space(lhs_parts[0])
-    lhs_parts[1] = strip_space(lhs_parts[1])
-    rhs = strip_space(rhs)
-    return ((lhs_parts[0], lhs_parts[1]), rhs)
